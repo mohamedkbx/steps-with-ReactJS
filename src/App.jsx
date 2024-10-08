7957ce9768e074e7bc11b3db0a2f95bb0146ca24
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-10-08 17:50:47 +0300

    feat: handle state using useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
+import { useState } from "react";
 import "./index.css";
 
 const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new income 🤑"];
 
 function App() {
-  let step = 2;
+  const [step, setStep] = new useState(1);
 
-  function handlePrevious() {}
-  function handleNext() {}
+  function handlePrevious() {
+    if (step > 1 && step <= 3) {
+      setStep(step - 1);
+    }
+  }
+  function handleNext() {
+    if (step >= 1 && step < 3) {
+      setStep(step + 1);
+    }
+  }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
         Step {step}: {messages[step - 1]}
