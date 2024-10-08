c6b6343930409ac3e87222022681d60b9d3bf586
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-10-08 14:32:16 +0300

    feat: Handle Events in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import "./index.css";
 
+const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new income 🤑"];
+
 function App() {
-  return <></>;
+  let step = 2;
+
+  function handlePrevious() {}
+  function handleNext() {}
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlePrevious}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleNext}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
