5b97a6a91a102d9e266fd1df1cd2bdd0a85aa31c
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-10-08 18:33:55 +0300

    conditonal redndeing depend on state

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new 
 
 function App() {
   const [step, setStep] = new useState(1);
+  const [isOpen, setIsOpen] = new useState(true);
 
   function handlePrevious() {
     if (step > 1 && step <= 3) {
@@ -18,24 +19,31 @@ function App() {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlePrevious}>
-          Previous
-        </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleNext}>
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlePrevious}>
+              Previous
+            </button>
+            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
